5f8c26e6293df611aa96435ebae2f47ebac36054
Renderizado40 <rosa_18@alu.uabcs.mx>
2020-11-12 22:03:35 -0700

    aloha hawaii

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+	$name="Roberto";
+	$pre= "Hola mi nombre es $name";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Page
+	</title>
+</head>
+<body>
+	<h1>
+		<?= $pre; ?>
+	</h1>
+</body>
+</html>
